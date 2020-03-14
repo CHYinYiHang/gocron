dedb1b35439ed407cb6c03a7cea8a6d7a2ea5a2d
CHYinYiHang <13393322330@163.com>
2020-03-15 00:48:39 +0800

    fist

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/CHYinYiHang/gocron
+
+go 1.14
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"github.com/CHYinYiHang/gocron/test"
+)
+
+func main()  {
+	fmt.Println("hello")
+	test.Test()
+}
